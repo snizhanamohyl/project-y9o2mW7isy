7e8523e963b84022e56bd1dd6b290abbbfa0e9dd
Snizhana Mohyl <snizhana.mogyl@gmail.com>
2023-08-17 20:35:48 +0300

    fix: BrowserRouter basename

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/project-y9o2mW7i">
+        <BrowserRouter basename="/project-y9o2mW7isy">
           <App />
         </BrowserRouter>
       </PersistGate>
